2a97e67ec59d9316a2e9888127befaada13e3017
Mariusz Drabik <drabik.mariusz@gmail.com>
2022-02-10 19:55:44 +0100

    controles plus gui

--- controler.py
+++ controler.py
@@ -1,10 +1,11 @@
-from sqlalchemy import create_engine
+from http.client import NETWORK_AUTHENTICATION_REQUIRED
+from msilib.schema import Error
+from googletrans import Translator
+from sqlalchemy import create_engine, select, true, update, insert
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-engine = create_engine(f'sqlite:///database/word.db')
-
+from random import randint
 
 Base = declarative_base()
 
@@ -20,17 +21,113 @@ class Word(Base):
 
     def __repr__(self):
         return (f"<User(en='{self.en_word}', pl='{self.pl_word}',"
-                f"próby='{self.trials}')>")
+                f"próby='{self.trials}', {self.known})>")
+
+    def get_tuple(self):
+        return (self.id, self.en_word, self.pl_word, self.trials)
+
+
+class Conection:
+
+    def session_maker(self):
+        engine = create_engine(f'sqlite:///database/word.db')
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)
+        return session()
+
+
+class FlashCard:
+
+    def save_new_card(self, en_word, pl_word):
+        with Conection().session_maker() as s:
+            statement = insert(Word).values(en_word=en_word, pl_word=pl_word)
+            s.execute(statement)
+            s.commit()
+        return f'Zapisano parę:\n{pl_word} - {en_word}'
+
+    def get_unknown_words(self):
+        with Conection().session_maker() as s:
+            statement = select(Word).filter_by(known=False)
+            result = s.execute(statement).all()
+            return result
+
+    def check_if_exists(self, word):
+        with Conection().session_maker() as s:
+            statement = select(Word).filter_by(pl_word=word)
+            result = s.execute(statement).all()
+            if result:
+                return 1
+            return 0
+
+    def update_word(self, word, known=False):
+        word_id = word.id
+        trials = word.trials
+        with Conection().session_maker() as s:
+            statement = update(Word).where(Word.id == word_id).\
+                values(trials=trials, known=known)
+            s.execute(statement)
+            s.commit()
+
+    def translate_word(self, word):
+        if not self.check_if_exists(word):
+            translator = Translator()
+            translation = translator.translate(word, src="pl", dest="en")
+            save = self.save_new_card(translation.text.lower(), word.lower())
+            return save
+        return 'Te słowo istnieje już w bazie'
+
+    def batch_translate_word(self, file):
+        try:
+            with open(file, 'r', encoding='utf-8') as file:
+                for word in file:
+                    word = word.strip()
+                    self.translate_word(word)
+        except Error as e:
+            print('Coś poszło nie tak', e)
+            return 'Nie zapisano'
+        return 'Zapisano'
+
+
+class Learnig:
+    def __init__(self):
+        self.word = self.random_word()
+
+    def random_word(self) -> Word:
+        words = FlashCard().get_unknown_words()
+        amount = len(words)-1
+        try:
+            if amount <= 1:
+                print(words)
+                word = words[0]
+                self.word = word[0]
+                return self.word
+        except IndexError as e:
+            print('Brak słów w bazie', e)
+            return -1
+        number = randint(0, amount)
+        word = words[number]
+        self.word = word[0]
+        return self.word
+
+    def show_word(self):
+        word = self.word.pl_word
+        return word
+
+    def check_result(self, word):
+        if word.lower() == self.word.en_word.lower():
+            self.word.trials += 1
+            if self.word.trials == 10:
+                self.word.known = True
+                FlashCard().update_word(self.word, known=self.word.known)
+                return 'Super słowo zapamiętane'
+            FlashCard().update_word(self.word)
+            return 'Brawo udało się zgadnąć słowo'
+        else:
+            return (f'Nie udało się słowo\n{self.word.pl_word}\n'
+                    f'w języku angielskim to:\n{self.word.en_word}')
 
 
 if __name__ == '__main__':
-    Base.metadata.create_all(engine) 
-    
-    Session = sessionmaker(bind=engine)
-    
-    session = Session()
-
-    word = Word(en_word='cat', pl_word='kot')
-    session.add(word)
-    session.commit()
-    print(word)
+    translator = Translator()
+    translation = translator.translate("Czeremcha", src='pl')
+    print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from cmath import inf
 import tkinter as tk
+from tkinter import filedialog as fd
+from controler import Learnig, FlashCard
 
 
 class MainView(tk.Frame):
@@ -8,41 +11,145 @@ class MainView(tk.Frame):
         self.layout_config()
         self.layout()
         self.config_wigets()
+        self.card = None
 
     def layout(self):
-        self.master.geometry('600x450+0+0')
-        self.master.grid()
-        self.master.grid_columnconfigure((0, 1, ), weight=1)
+        self.master.geometry('350x550+0+0')
+        # self.master.pack()
+        # self.master.grid_columnconfigure((0, 1, ), weight=1)
+
+        self.get_word = tk.Button(self.master, text='Losuj fiszkę',
+                                  command=self.get_flash_card)
+        self.get_word.pack(padx=10, pady=10, ipadx=2, ipady=2,)
 
         self.option_label = tk.Label(self.master, text='Przetłumacz słowo:')
-        self.option_label.grid(row=1, column=0, padx=0, ipadx=0, pady=(22, 2))
+        self.option_label.pack(pady=(22, 22))
 
         self.word_p_label = tk.Label(self.master, text='Wylosowane słowo')
-        self.word_p_label.grid(row=2, column=0, padx=0, ipadx=0, pady=(2, 2))
+        self.word_p_label.pack(pady=(12, 22))
 
-        self.add_project_entry = tk.Entry(self.master)
-        self.add_project_entry.grid(row=2, column=1, padx=2, ipadx=2, ipady=5,)
+        self.answer = tk.Entry(self.master)
+        self.answer.pack(pady=(12, 22), ipadx=2, ipady=5,)
+        self.answer.bind('<Return>', lambda e: self.check_answer(e))
 
-        self.add_button = tk.Button(self.master, text='Sprawdź',
-                                    command=self.place_holder)
-        self.add_button.grid(row=3, padx=10, pady=10, ipadx=2, ipady=2,
-                             column=0, columnspan=2)
+        self.check_button = tk.Button(self.master, text='Sprawdź',
+                                    command=self.check_answer)
+        self.check_button.pack(padx=10, pady=10, ipadx=2, ipady=2,)
+        
+        self.messages = tk.Message(self.master, text=' ')
+        self.messages.pack(pady=(12, 22))
+
+        self.option = tk.Button(self.master, text='Opcje',
+                                command=lambda: self.open_options(OptionWindow))
+        self.option.pack(padx=10, pady=10, ipadx=2, ipady=2,)
 
     def config_wigets(self):
-        self.master.configure(bg='#333', relief='flat', padx=10, pady=10)
-
-        self.option_label.config(**self.options_labels, width=15,)
-        self.word_p_label.config(bg='#333', fg=self.fg, relief='flat',
-                                 font=self.font, width=15,)
-
-        self.add_project_entry.config(**self.options, width=29,)
-        self.add_button.config(**self.options, width=25,)
+        self.master.configure(bg='#111', relief='flat', padx=10, pady=10)
+        self.get_word.config(**self.options, width=30,)
+        self.option_label.config(**self.options_labels, width=30,)
+        self.word_p_label.config(bg='#111', fg=self.fg, relief='flat',
+                                 font=self.font, width=30,)
+        self.answer.config(**self.options, width=30,)
+        self.check_button.config(**self.options, width=30,)
+        self.messages.config(bg='#111', fg=self.fg, relief='flat',
+                             font=self.font, width=300, justify='center')
+        self.option.config(**self.options, width=30,)
 
     def layout_config(self):
         self.master.title('Fiszki')
 
         self.font = ('helvetica', 12)
-        self.font_labels = ('helvetica', 11, 'italic')
+        self.font_labels = ('helvetica', 11)
+        self.fg = '#f1f1f1'
+        self.bg = '#444'
+        self.options = dict(bg=self.bg, fg=self.fg,
+                            relief='flat', font=self.font)
+        self.options_labels = dict(bg='#111', fg=self.fg,
+                                   relief='flat', font=self.font_labels)
+
+    def get_flash_card(self):
+        self.card = Learnig()
+        word = self.card.show_word()
+        self.messages.configure(text=' ')
+        print(word)
+        self.word_p_label.config(text=word)
+
+    def check_answer(self, event=None):
+        if not self.card:
+            self.messages.config(text='Najpierw wylosuj fiszkę')
+            return 0
+        ansewer = self.answer.get()
+        self.answer.delete(0, 'end')
+        message = self.card.check_result(ansewer)
+        self.messages.config(text=message)
+        print(ansewer)
+
+    def open_options(self, _class):
+        self.new = tk.Toplevel(self.master)
+        _class(self.new)
+
+    def do_nothing(self):
+        self.master.destroy()
+        print('Zrobione coś zanim zostało zamknięte :P')
+
+
+class OptionWindow(tk.Toplevel):
+    def __init__(self, master):
+        print(myapp.new.state())
+        self.master = master
+        self.layout_config()
+        self.layout()
+        self.config_wigets()
+        self.file = tk.StringVar
+
+    def layout(self):
+        self.master.geometry('350x550+0+0')
+
+        self.info = "Dodaj słowo w języku polskim. Automatycznie przetłumaczymy te słowo i dodamy do bazy"
+
+        self.info_label = tk.Message(self.master, text=self.info)
+        self.info_label.pack(pady=(22, 22))
+
+        self.word_to_add = tk.Entry(self.master)
+        self.word_to_add.pack(pady=(12, 22), ipadx=2, ipady=5,)
+        self.word_to_add.bind('<Return>', lambda e: self.save_word(e))
+
+        self.add_button = tk.Button(self.master, text='Dodaj',
+                                    command=self.save_word)
+        self.add_button.pack(padx=10, pady=10, ipadx=2, ipady=2,)
+
+        self.messages = tk.Message(self.master, text=' ')
+        self.messages.pack(pady=(12, 22))
+
+        self.info_2 = '''Możesz także dodać wiele słów naraz wczytaj poniżej plik z rozszerzeniem txt, gdzie każdy wyraz będzie zapisany w osobnej linii'''
+
+        self.info_2_label = tk.Message(self.master, text=self.info_2)
+        self.info_2_label.pack(pady=(22, 22))
+
+        self.add_multi_button = tk.Button(self.master, text='Dodaj wiele',
+                                          command=self.batch_save)
+        self.add_multi_button.pack(padx=10, pady=10, ipadx=2, ipady=2,)
+        
+        self.status_label = tk.Label(self.master, text='')
+        self.status_label.pack(pady=(22, 22))
+
+    def config_wigets(self):
+        self.master.configure(bg='#333', relief='flat', padx=10, pady=10)
+        self.info_label.config(**self.options_labels, width=300,
+                                 justify='center')
+        self.word_to_add.config(**self.options, width=30,)
+        self.add_button.config(**self.options, width=30,)
+        self.messages.config(bg='#333', fg=self.fg, relief='flat',
+                             font=self.font, width=300, justify='center')
+        self.info_2_label.config(**self.options_labels, width=300,
+                                 justify='center')
+        self.add_multi_button.config(**self.options, width=30,)
+        self.status_label.config(**self.options_labels, width=30,)
+
+    def layout_config(self):
+        self.master.title('Fiszki - opcje')
+        self.font = ('helvetica', 12)
+        self.font_labels = ('helvetica', 11)
         self.fg = '#f1f1f1'
         self.bg = '#444'
         self.options = dict(bg=self.bg, fg=self.fg,
@@ -50,12 +157,15 @@ class MainView(tk.Frame):
         self.options_labels = dict(bg='#333', fg=self.fg,
                                    relief='flat', font=self.font_labels)
 
-    def place_holder(self):
-        pass
-
-    def do_nothing(self):
-        self.master.destroy()
-        print('Zrobione coś zanim zostało zamknięte :P')
+    def save_word(self, event=None):
+        word = self.word_to_add.get()
+        message = FlashCard().translate_word(word)
+        self.messages.configure(text=message)
+    
+    def batch_save(self):
+        self.file = fd.askopenfilename()
+        info = FlashCard().batch_translate_word(self.file)
+        self.status_label.config(text=info)
 
 
 if __name__ == '__main__':
